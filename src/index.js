aea727915ade1fcc34e18902839f93eae166bdd5
unknown <kymkhw279@gmail.com>
2023-12-22 17:17:43 +0900

    update

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+
+if (process.env.NODE_ENV === "production") disableReactDevTools();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
